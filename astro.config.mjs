3d69454ee7683a7f798356d5de82606b2e736c07
Devon Cash <devon.p.cash@gmail.com>
2023-06-30 13:26:17 -0700

    Making adjustments to try to get build working

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    redirects: true
-  },
   output: 'hybrid',
   site: 'https://corvallisalt.org',
-  redirects: {
-    '/': '/events'
-  },
   adapter: cloudflare()
 });
